103586bbeb4e4297000f4d7d5c9fe51ab9cbe8af
Nitro <66451996+N1-TR0@users.noreply.github.com>
2022-01-12 16:02:05 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const config = require("./config.json");
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", () => {
+  console.log("bot is ready");
+});
+
+client.on("messageCreate", (message) => {
+  console.log("message sent");
+});
+
+client.login(config.token);
