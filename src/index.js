1b37b1d6d4d7047b400e390c63b9007e818d3ae5
Nitro <66451996+N1-TR0@users.noreply.github.com>
2022-01-15 13:14:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on("ready", () => {
-  console.log("bot is ready");
+  console.log("bot is online");
 });
 
 client.on("messageCreate", (message) => {
   console.log("message sent");
 });
 
-client.login(config.token);
+client.login('token here');
